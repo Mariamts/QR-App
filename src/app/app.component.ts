5c6c964d75d69a523c9773b306a9bc84957a669e
mariami <mariam.tsotsolashvili@geolab.edu.ge>
2023-02-20 18:40:43 +0400

    feat: Add QR code that redirects to Facebook

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as QRCode from 'qrcode';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'facebook-qr-app';
+  ngOnInit() {
+    const url = 'https://www.facebook.com';
+    QRCode.toDataURL(url, (err, qrCodeUrl) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const qrCodeImg = document.getElementById('qrcode');
+        qrCodeImg!.setAttribute('src', qrCodeUrl);
+      }
+    });
+  }
 }
